011100d673c92179a618d4c7b78a1f8e6962c68c
Martin Marchese <martin@MacBook-Pro-de-Martin.local>
2024-09-17 23:32:42 -0300

    initial site content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RouterLink } from '@angular/router';
+import { RouterModule } from '@angular/router';
+import { NavbarComponent } from "./navbar/navbar.component";
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, RouterModule, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,31 @@
-import { Routes } from '@angular/router';
+import { PRIMARY_OUTLET, Routes } from '@angular/router';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { EducationComponent } from './education/education.component';
+import { SkillsComponent } from './skills/skills.component';
+import { InterestsComponent } from './interests/interests.component';
+import { ContactComponent } from './contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+{
+    path: '', component: AboutMeComponent, outlet: PRIMARY_OUTLET
+},
+{
+    path: 'about-me', component: AboutMeComponent, outlet: PRIMARY_OUTLET
+},
+{
+  path: 'experience', component: ExperienceComponent, outlet: PRIMARY_OUTLET
+},
+{
+  path: 'education', component: EducationComponent, outlet: PRIMARY_OUTLET
+},
+{
+  path: 'skills', component: SkillsComponent, outlet: PRIMARY_OUTLET
+},
+{
+  path: 'interests', component: InterestsComponent, outlet: PRIMARY_OUTLET
+},
+{
+  path: 'contact', component: ContactComponent, outlet: PRIMARY_OUTLET
+}
+];
